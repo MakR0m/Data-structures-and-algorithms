b129939a98e916f765c22fabf3b91e0594d1822a
Roman <Lamb_54@mail.ru>
2025-04-08 12:16:10 +0700

    - nullable - класс, структура, рекорд, енум - боксинг, анбоксинг

--- Vulues types and Reference types/Program.cs
+++ Vulues types and Reference types/Program.cs
@@ -3,36 +3,57 @@ using System.Text;
 
 namespace Vulues_types_and_Reference_types
 {
+    //Когда использовать класс, когда рекорд, когда стракт
+    // enum — если значения ограничены и логически предопределены (дни недели, статусы, роли, флаги и т.п.)
+    // struct — если объект: маленький, часто копируется, не требует ссылочного поведения (координаты, цвета, временные точки)
+    // class — почти во всех остальных случаях, объект с логикой
+    // record - хранение данных, DTO, модели ответа API
+
+    //C C#9/.NET 5+ record - это специальный вид class или struct, предназначенный для хранения неизменяемых данных, с автоматическим сравнением по значениям и поддержкой deconstruct.
+    //Отличия от класса. Сравнение по значению, автосоздание ToString(), Equals(), GetHashCode(), поддержка with-выражения, иммутабельность по умолчанию  (у init)
+
+    record Person1(string Name, int Age);         // Reference type
+    record struct Point1(int X, int Y);           // Value type (C# 10+)/  У record struct оператор == не определён по умолчанию (в отличие от record class). Чтобы сравнить record struct, нужно использовать .Equals()
+
+
+    enum DayOfWeek
+    {
+        Monday,    // 0   Используется для ограниченного набора значений. Значимый тип, копируется при передаче
+        Tuesday,   // 1   По умолчанию enum основан на int, но можно задать другой тип (byte, short и тд)
+        Wednesday, // 2   Значения можно приводить к числам и обратно
+        Thursday = 10,
+        Friday,    // 11
+    }
+
+    struct Point                         //Хранится в стеке (если не вложен в объект)
+    {                                    //Копируется при передаче
+        public int X;                    //Не может иметь наследование
+        public int Y;                    //Подходит для маленьких НЕИЗМЕНЯЕМЫХ объектов (Point, DateTime, Color)
+        public Point(int x, int y)       //Может иметь конструкторы и методы, но без явного конструктора по умолчанию
+        {
+            X = x;
+            Y = y;
+        } 
+
+    }
+
+    class Person                       //Хранится в куче, а в переменной — ссылка
+    {                                  //Передаётся по ссылке
+        public string Name;            //Поддерживает наследование, полиморфизм
+        public int Age;                //Используется везде, где есть сложная логика или изменяемые данные
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            //TypesAndStringWork();
-            // VarObjDyn();
-            object obj = 10;
-            dynamic dyn = obj;
+            TypesAndStringWork();
+            VarObjDyn();
+            Nullable();
+            ClassStructRecord();
+            BoxUnbox();
 
-            int result = dyn + 5; //dyn становится dynamic, но указывает на int. C# "разворачивает" это в рантайме, как будто 10 + 5
-            Console.WriteLine(result);
-
-
-
-        }
-
-        private static void VarObjDyn()
-        {
-            //var - неявный, но строготипизированный. Тип определяется на этапе компиляции. Ничего общего с динамическими типами не имеет. Просто сахар
-            //object - хранит любой тип, т.к. все наследуются от класса object, но при использовании нужно приведение
-            //object o = 123; int x = (int)o;
-            // InvalidCastExeption ошибка приведения.
-
-            //dynamic - тип определяемый в рантайме. Во время выполнения
-
-            dynamic d = 5;
-            d.Method(); //Компилятор не проверяет, есть ли у обьекта метод или свойство.В случае ошибки выкидывает ошибку во время выполнения.
-            Console.WriteLine(d + 3); // 8
-
-            //Когда использовать: var - когда тип очевиден, object - редко, обычно в универсальных коллекциях, старом API, dynamic - при работе с JSON, COM, Reflection, ExpandoObject, осторожно в бизнес логике (нет проверок)
         }
 
         private static void TypesAndStringWork()
@@ -116,6 +137,93 @@ namespace Vulues_types_and_Reference_types
                 builder.Append(i);
             }
             string f = builder.ToString();
+        }
+        
+        private static void VarObjDyn()
+        {
+            //var - неявный, но строготипизированный. Тип определяется на этапе компиляции. Ничего общего с динамическими типами не имеет. Просто сахар
+            //object - хранит любой тип, т.к. все наследуются от класса object, но при использовании нужно приведение
+            //object o = 123; int x = (int)o;
+            // InvalidCastExeption ошибка приведения.
+
+            //dynamic - тип определяемый в рантайме. Во время выполнения
+
+            //dynamic d = 5;
+            //d.Method(); //Компилятор не проверяет, есть ли у обьекта метод или свойство.В случае ошибки выкидывает ошибку во время выполнения.
+            //Console.WriteLine(d + 3); // 8
+
+            //Когда использовать: var - когда тип очевиден, object - редко, обычно в универсальных коллекциях, старом API, dynamic - при работе с JSON, COM, Reflection, ExpandoObject, осторожно в бизнес логике (нет проверок)
+
+            object obj = 10;
+            dynamic dyn = obj;
+
+            int result = dyn + 5; //dyn становится dynamic, но указывает на int. C# "разворачивает" это в рантайме, как будто 10 + 5
+            Console.WriteLine(result);
+        }
+
+        private static void Nullable()
+        {
+            //По умолчанию значимые типы не могут быть null, у них есть значения по умолчанию.
+            //Nullable<T> - int?, bool?
+
+            int? age = null;
+            if (age.HasValue)
+                Console.WriteLine(age.Value);
+            else
+                Console.WriteLine("Возраст не найден");
+            //Лучше и безопаснее использовать age?? или age.GetValueOrDefault();
+            //Оператор ?? - "Если null, то..."
+            int? age1 = null;
+            int actualAge = age ?? 18; // если age == null, то возьмём 18
+            //Оператор ??= - "Установи значение, если переменная null"
+            string? name = null;
+            name ??= "Гость";   // если имя пустое, то запиши "Гость"
+            Console.WriteLine(name);
+
+            //оператор ?. - безопасный доступ
+            Person? p = null;
+            Console.WriteLine(p?.Name); //Не вызовет NullReferenceException.Если p == null, выражение возвращает null, не упадёт с исключением.
+            //Полезно в работе с базами данных(int? из SQL), при парсинге JSON, XML, когда есть опциональные поля, в моделях(например, пользователь без email)
+
+            string? input = null;
+            string result = input?.ToUpper() ?? "DEFAULT";  // result вернул пусто во время приведение к верхнему регистру, потому что стоит ?., а раз нал, то значение "DEFAULT"
+
+            Console.WriteLine(result);
+
+        }
+
+        private static void ClassStructRecord()
+        {
+            DayOfWeek en = DayOfWeek.Monday; //Значения можно приводить к числам и обратно
+            Console.WriteLine((int)en);
+            DayOfWeek en1 = (DayOfWeek)10;
+            Console.WriteLine(en1);
+
+            var p1 = new Person1("Anna", 30);
+            var p2 = new Person1("Anna", 30);
+
+            Console.WriteLine(p1 == p2); // true! сравнение по значению
+
+            var p3 = p1 with { Age = 31 };
+            Console.WriteLine(p3); // Person { Name = Anna, Age = 31 }
+        }
+        
+        private static void BoxUnbox()   
+        {
+            int i = 42;
+            object o = i; // ← boxing происходит здесь. Преобразование value type (например, int, double) в object, то есть упаковка значения в ссылку. Значение 42 упаковывается в новый объект в куче (heap).
+            int j = (int)o; // ← unboxing. Обратный процесс: извлечение value type из object
+
+            // Boxing → создаёт объект в куче. Unboxing → требует точного приведения типа
+            // Это неявное выделение памяти + может быть медленным, особенно в циклах или при работе с коллекциями
+
+            // Boxing происходит: при присваивании value type переменной типа object или interface
+            // При передаче value type в метод, принимающий object
+            // При добавлении value type в старые коллекции (ArrayList, Hashtable - не дженерики хранят все как object). List<T> избегает лишней запаковки, распаковки
+
+            //object o = 42; Unboxing требует точного указания типа, какой тип запаковали, тот и должны указать при распаковке, иначе ошибка.
+            //int x = (int)o;    ✅
+            //long y = (long)o;  ❌ InvalidCastException
         }
     }
 }
